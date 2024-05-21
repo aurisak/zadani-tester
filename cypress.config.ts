410494c7bdc2f636dd994882ba42ade06653963c
Jana Kantorová <Janca.kantoj@gmail.com>
2024-05-21 19:02:48 +0200

    fix sentry security error - set chromeWebSecurity to false

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  // fix the __SENTRY__ security error
+  chromeWebSecurity: false,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
